ddb65927ae61fa250132d245c7bcb186cde816f7
Gorka <gorka@hey.com>
2024-11-20 19:18:34 -0600

    scaffold pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_21_011721) do
+  create_table "pages", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
